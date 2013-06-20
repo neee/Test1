ca95909afe880eebfd5acc88912afc217fe44458
neee <itneee@gmail.com>
2013-06-21 00:21:59 +0400

    new activity

---
+++ src/com/example/Test1/Droid2.java
@@ -0,0 +1,19 @@
+package com.example.Test1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neee
+ * Date: 21.06.13
+ * Time: 0:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Droid2 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.second);
+        super.onCreate(savedInstanceState);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
